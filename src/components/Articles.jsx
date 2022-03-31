888fbc3a1398a3cedb213d3ecdffeab6ae177ec3
Akshada Shelke <84895276+Akshada13@users.noreply.github.com>
2022-03-31 22:48:40 +0530

    Add files via upload

---
+++ src/components/Articles.jsx
@@ -0,0 +1,39 @@
+// eslint-disable-next-line
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { getNews } from "../services/api";
+import Article from "./Article";
+
+const Articles = () => {
+  const [news, setNews] = useState([]);
+  const [page, setPage] = useState(0);
+
+
+  useEffect(() => {
+    dailyNews();
+  }, [page])// eslint-disable-line react-hooks/exhaustive-deps
+  
+
+  const dailyNews = async () => {
+    let response = await getNews(page);
+    setNews([...news,...response.data]);
+    console.log(response);
+  };
+
+  return (
+    <InfiniteScroll
+        dataLength={news.length}
+        next={() => setPage(page => page + 1)}
+        hasMore={true}
+    >
+        {
+            news.map(article => (
+                <Article article={article} />
+            ))
+        }
+    </InfiniteScroll>
+)
+}
+
+export default Articles;
